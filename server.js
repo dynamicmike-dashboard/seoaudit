f2d4692cb6ab789a75d9935cf4129c40b6066888
dynamicmike-dashboard <dynamicmike@gmail.com>
2026-07-28 12:53:43 -0500

    wrap dotenv in try-catch

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+try { require('dotenv').config(); } catch(e) {}
 const express = require('express');
 const nodemailer = require('nodemailer');
 const path = require('path');
